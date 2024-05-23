d8476b158ceb10d86c33b29726cb897816ddc0ee
Gvivi <viviane@guenth.art>
2024-05-23 15:01:09 +0200

    added argument save_steps

--- train.py
+++ train.py
@@ -255,11 +255,11 @@ if __name__ == "__main__":
     parser.add_argument("--test_iterations", nargs="+", type=int, default=[7_000, 30_000])
     parser.add_argument("--test_steps", type=int, default=1000)
     parser.add_argument("--save_iterations", nargs="+", type=int, default=[7_000, 30_000])
+    parser.add_argument("--save_steps", type=int)
     parser.add_argument("--quiet", action="store_true")
     parser.add_argument("--checkpoint_iterations", nargs="+", type=int, default=[])
     parser.add_argument("--start_checkpoint", type=str, default = None)
     args = parser.parse_args(sys.argv[1:])
-    args.save_iterations.append(args.iterations)
 
     start_time = time.time()
     
@@ -268,6 +268,12 @@ if __name__ == "__main__":
     args.test_iterations = list(range(0, args.iterations +1, args.test_steps))
     if args.test_iterations[-1] != args.iterations:
         args.test_iterations.append(args.iterations)
+
+    if args.save_steps:
+        args.save_iterations = list(range(0, args.iterations +1, args.save_steps))
+    
+    if args.save_iterations[-1] != args.iterations:
+        args.save_iterations.append(args.iterations)
 
     # Initialize system state (RNG)
     safe_state(args.quiet)
